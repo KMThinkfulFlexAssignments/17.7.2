7783cfc8151846f8562f860f85383890a057d0fa
Kevin Moore <moore.kevin.p@gmail.com>
2020-03-17 11:46:29 -0400

    initial commit to prep for deployment

---
+++ server.js
@@ -0,0 +1,55 @@
+'use strict';
+
+require('dotenv').config();
+
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const MOVIEDEX = require('./moviedex.json');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  next();
+});
+
+app.get('/movie', function handleGetMovies(req, res) {
+  let response = MOVIEDEX;
+
+  if (req.query.genre) {
+    response = response.filter(movies => 
+      movies.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    );
+  }
+
+  if (req.query.country) {
+    response = response.filter(movies =>
+      movies.country.toLowerCase().includes(req.query.toLowerCase().country)
+    );
+  }
+
+  if (req.query.avg_vote) {
+    response = response.filter(movies =>
+      Number(movies.avg_vote) >= Number(req.query.avg_vote)
+    );
+  }
+
+  res.json(response);
+});
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
